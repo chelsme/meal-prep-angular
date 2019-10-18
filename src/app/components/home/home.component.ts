b1b8cefee99092f0bdba9e832b055cd0d7dc0aaf
Chelsea Crowson <chelsea.crowson@accenture.com>
2019-10-18 11:44:10 -0500

    remove extra code

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -21,7 +21,6 @@ export class HomeComponent implements OnInit, DoCheck {
       this.userService.fetchUsers();
       this.users = this.userService.users;
       this.createChart();
-      // this.test();
     }
 
     if (this.users && this.users.length !== this.userService.users.length) {
@@ -58,29 +57,12 @@ export class HomeComponent implements OnInit, DoCheck {
       circles
         .enter()
         .append('circle')
-        // .transition()
-        // .duration(750)
-        // .ease(d3.easeLinear)
-        // .attr('cx', d => d.id * 50 + 150)
-        // .attr('cy', d => (d.recipes.length + 2) * 50)
-        // .attr('r', d => (d.recipes.length + 2) * 5)
         .attr('r', (d) => radiusScale(d.recipes.length))
         .style('fill', function() {
           return 'hsl(' + Math.random() * 360 + ',100%,50%)';
         });
 
       simulation.nodes(this.users).on('tick', () => this.ticked());
-
-      circles.each;
-
-      // canvas
-      //   .selectAll('text')
-      //   .data(this.users)
-      //   .enter()
-      //   .append('text')
-      //   .attr('x', d => d.id * 50 + 130)
-      //   .attr('y', d => (d.recipes.length + 2) * 50)
-      //   .text(d => d.name);
     }
   }
 
@@ -88,7 +70,5 @@ export class HomeComponent implements OnInit, DoCheck {
     var canvas = d3.select('#test');
     var circles = canvas.selectAll('circle').data(this.users);
     circles.attr('cx', (d) => d.x).attr('cy', (d) => d.y);
-    // .attr('cx', d => d.id * 50 + 150)
-    // .attr('cy', d => (d.recipes.length + 2) * 50);
   }
 }
